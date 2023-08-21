2c32cc2f8f4db6ca10c51b5a758fd05c0306cf16
Amagreat <gloryagbai3@gmail.com>
2023-08-21 16:07:49 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main."
+/**
+ * puts2 - prints every other character
+ * starting with the first character
+ * @str: input string
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int length = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		length++;
+	}
+
+	for (o = 0; o < length; o += 2)
+	{
+		_putchar(str[o]);
+	}
+	_putchar('\n');
+}
+
